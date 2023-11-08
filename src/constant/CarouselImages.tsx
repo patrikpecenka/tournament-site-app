9e730fb514fda3801a6f012d52a8956ca0f8e4d0
patrikpecenkae <ppecenka@icloud.com>
2023-11-08 18:27:28 +0100

    .

--- src/constant/CarouselImages.tsx
+++ src/constant/CarouselImages.tsx
@@ -3,7 +3,7 @@ import rocketleague from "/rocketleague.jpg"
 import overwatch from "/overwatch-2.jpg"
 import csgo from "/csgo.jpg"
 import valorant from "/valorant.jpg"
-import background from "/background.jpg"
+import background from "/background.png"
 
 
 export const images = [
